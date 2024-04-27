b51ea0446aa9d5913a0d405512223c55d3df354e
ivan <ivan@MacBook-Pro-ivan.local>
2024-04-27 14:16:02 +0300

    first commit

---
+++ part_2.py
@@ -0,0 +1,11 @@
+# start = int(input("Введите начальное значение диапазона: "))
+# end = int(input("Введите конечное значение диапазона: "))
+# total_sum = 0
+# for num in range(start, end + 1):
+#     total_sum += num
+# print("Сумма чисел в диапазоне от", start, "до", end, ":", total_sum)
+
+# uset_name = None
+# while uset_name != "q":
+#     uset_name = input("You name: ")
+#     print(uset_name)
